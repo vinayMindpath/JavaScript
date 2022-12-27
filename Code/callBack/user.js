26161553a20ed946fcc16c606885ed9aa73bc988
vinaypanwar280@gmail.com <vinay@mindpathtech.com>
2022-12-27 16:58:16 +0530

    Undo

---
+++ Code/callBack/user.js
@@ -0,0 +1,38 @@
+// import { start } from "repl";
+
+// import { title } from "process";
+
+function login(email,pass,callback){
+    setTimeout(() => {
+        return callback({userEmail : email});
+    }, 3000);
+}
+
+function getvideos(email,callback){
+    setTimeout(() => {
+        return callback(["video1","video2","video3"]);
+    }, 2000);
+}
+
+function videoDetials(video,callback){
+    setTimeout(() => {
+        return callback("this is title of the "+video);
+    }, 2000);
+}
+console.log('start');
+
+const email = login("vinay@mindpathtech.com", 2323,user=>{
+    console.log(user);
+    getvideos(user.userEmail,videos=>{
+        console.log(videos);
+        videos.forEach(element => {
+            videoDetials(element,title=>{
+                console.log(title);
+            });
+        });
+    });
+    return user; 
+});
+// const videos = getvideos(email);
+
+// console.log(videos);
