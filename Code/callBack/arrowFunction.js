b4cae8ec53ebe27ca7596b07367ce6110c18b892
= <=vinay@mindpathtech.com>
2022-12-28 12:25:52 +0530

    node module moved and Event looping directory is added

--- Code/callBack/arrowFunction.js
+++ Code/callBack/arrowFunction.js
@@ -1,22 +1,22 @@
-const anotherFunction = fn=>{
-    console.log("another function run")
-    console.log(" in another function")
-    hold = fn(100)
-    console.log("Hold : ",hold)
-    return hold;
+const anotherFunction = (fn) => {
+  console.log("another function run");
+  console.log(" in another function");
+  hold = fn(100);
+  console.log("Hold : ", hold);
+  return hold;
 };
-const val = a => {
-    console.log('val function run. ')
-    console.log(" in a function.")
-    console.log(a*2);
-    return a*2;
+const val = (a) => {
+  console.log("val function run. ");
+  console.log("in a function.");
+  console.log(a * 2);
+  return a * 2;
 };
 
-console.log("start")
-anotherFunction(val)
-console.log("end")
+console.log("start");
+anotherFunction(val);
+console.log("end");
 
-let add = (a,b) => a+b;
-let another = get => get(1,2);
+let add = (a, b) => a + b;
+let another = (get) => get(1, 2);
 
 console.log(another(add));
