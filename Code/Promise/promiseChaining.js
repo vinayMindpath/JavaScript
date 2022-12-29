efc24cdfe32d8c288ba2ef195283d6c98ba4edc5
vinay-panwar <vinaypanwar280@gmail.com>
2022-12-29 09:06:24 +0530

    changes in promisechain2 and added npm with some functionality

--- Code/Promise/promiseChaining.js
+++ Code/Promise/promiseChaining.js
@@ -1,23 +1,23 @@
-let fetchData = function(data) {
-    return new Promise((res,err)=>{
+let fetchData = function() {
+    return new Promise((res, err) => {
         setTimeout(() => {
-           console.log("fetching data complete")
-           res({id:12,message : "nice work...."});
+            console.log("fetching data complete")
+            res({ id: 12, message: "nice work...." });
         }, 2000);
     })
 };
 
-let parseData = function (data){
+let parseData = function(data) {
     return new Promise(res => {
-        a = variable;
+        // a = variable;
         setTimeout(() => {
             let parsedOutput = `parsed the data for id : ${data.id} with message ${data.message}`;
-            res({parsed : parsedOutput});
+            res({ parsed: parsedOutput });
         }, 2000);
     })
 }
 
-let echoData =function (data){
+let echoData = function(data) {
     return new Promise(() => {
         setTimeout(() => {
             console.log(data.parsed)
@@ -25,7 +25,17 @@ let echoData =function (data){
     })
 }
 
-fetchData().then(parseData).then(echoData).catch(()=>{
-    // console.log("error encounterred...");
-    console.log(new Error)
-})
+// fetchData().then(data => {
+//     parseData(data).then(res => {
+//         echoData(res).then();
+//     });
+// })
+
+// fetchData().then(data).then(echoData(data)).catch(() => {
+//     console.log("error encounterred...");
+//     // console.log(new Error)
+// })
+
+fetchData().then(data =>
+    parseData(data)
+).then(res => echoData(res)).then()
