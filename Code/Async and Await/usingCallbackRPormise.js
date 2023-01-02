82c17b6ed4a95976590d63ec8a4b9e1118520795
vinayMindpath <vinay@mindpathtech.com>
2023-01-02 15:17:55 +0530

    new var and let folder added and using callback and promises in async and await

---
+++ Code/Async and Await/usingCallbackRPormise.js
@@ -0,0 +1,22 @@
+function multiply(a, b) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("got the result");
+      resolve(a * b);
+    }, 1000);
+  });
+}
+
+function compute(a, callback) {
+  return callback(a, a);
+}
+
+async function square(a) {
+  return await compute(a, multiply).then();
+}
+
+square(4).then((res) => {
+  console.log(res);
+});
+
+// this is an example where await is return a callback rather than promise but used an async keyword which alwasys return a promise
--- Code/var and let/diffVarLet.js
+++ Code/var and let/diffVarLet.js
@@ -1,10 +1,23 @@
-function example() {
-  let hello = 34;
+function exampleLet() {
+  let letHello = "assigned at the time of creation";
   if (true) {
     // hello = 45;
-    let hello = "121";
+    letHello = "changed in if block";
   }
-  console.log(hello);
+  //   console.log(hello);
+  return letHello;
 }
 
-example();
+console.log(`example of let : ${exampleLet()}`);
+// console.log(`accessing variable outside the scope of let :${letHello}`);
+
+function exampleVar() {
+  var varHello = "defined by var keyword";
+  if (true) {
+    varHello = "changed in if block";
+  }
+  return varHello;
+}
+
+console.log(`example of var : ${exampleVar()}`);
+console.log(`accessing variable outside the scope of var : ${varHello}`);
