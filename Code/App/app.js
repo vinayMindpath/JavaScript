0743cf92e645d711caeb43502965b9136637a0c3
vinayMindpath <vinay@mindpathtech.com>
2023-01-04 11:09:09 +0530

    App module is added

---
+++ Code/App/app.js
@@ -0,0 +1,7 @@
+// import { deepStrictEqual } from "assert";
+import { names } from "./storageApp.js";
+import { displayName } from "./storageApp.js";
+
+names.forEach((ele) => {
+  displayName(ele);
+});
